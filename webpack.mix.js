9a250627194f365eab62afe43499754af24d6ebe
7OMI <36586477+7OMI@users.noreply.github.com>
2023-10-17 20:28:03 +0200

    + Betűtípusok hozzáadása

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix
+    .options({
+        processCssUrls: false
+    })
     .js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy('resources/fonts/', 'public/fonts/', false)
 ;
